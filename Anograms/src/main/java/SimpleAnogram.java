159a1a4cf08098d4386f8157be48090e7989e02e
ASUS <ASUS@DESKTOP-5LUIJ39>
2022-09-02 21:16:22 +0300

    initial commit

---
+++ Anograms/src/main/java/SimpleAnogram.java
@@ -0,0 +1,21 @@
+
+public class SimpleAnogram {
+	
+	private String reverse (String inputString) {
+		String[] words = inputString.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			StringBuilder wordSB = new StringBuilder(words[i]);
+			StringBuilder reversedWordSB = wordSB.reverse();
+			words[i] = reversedWordSB.toString();
+		}
+
+		String reversedString = String.join(" ", words);
+		return reversedString;
+	}
+	
+	public static void main(String[] args) {
+		SimpleAnogram example = new SimpleAnogram();
+		System.out.println(example.reverse("abcd efgh"));
+	}
+
+}
